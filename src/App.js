a5a99037f404a187f1db20ca702d785a4c714288
=mplecapt <=mplecapt@mtu.edu>
2022-01-27 15:57:33 -0500

    Initial setup. Functional, not pretty

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import { Formik, Field, Form } from 'formik';
+import { wordlist as dict } from './wordlist';
+import { useState } from 'react';
+
+function check(val, msg) {
+	return (val && val.length > 0) ? msg : '';
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	let [regx, setRegx] = useState(new RegExp(''));
+	return (
+		<div className="App">
+			<Formik
+				initialValues={{
+					notInWord: '',
+					somewhere: '',
+					known: ['', '', '', '', ''],
+					butNotHere: ['', '', '', '', ''],
+				}}
+				validate={(values) => {
+					let notContain = check(values.notInWord, `(?!\\b\\w*[${values.notInWord}]\\w*\\b)`);
+					let sumwhere = check(values.somewhere, values.somewhere.split("").map(n => `(?=\\b\\w*${n}\\w*\\b)`).join(""));
+					let knownPos = ['', '', '', '', ''];
+					for (let i = 0; i < 5; i++) {
+						knownPos[i] = values.known[i] || (values.butNotHere[i] && `[^${values.butNotHere[i]}]`) || `\\w`;
+					}
+					
+					let tmp = `${notContain}${sumwhere}\\b${knownPos.join("")}\\b`;
+					//console.log(tmp);
+					setRegx(new RegExp(tmp));
+				}}
+			>
+				<Form>
+					<label htmlFor='notInWord'>Letters not in word </label>
+					<Field name='notInWord' /><br/>
+					<label htmlFor='somewhere'>Unkown positions </label>
+					<Field name='somewhere' /><br/>
+					<div>
+						<label htmlFor='known'>Known positions</label>
+						<Field type='text' style={{width: '20px'}} name={'known[0]'} />
+						<Field type='text' style={{width: '20px'}} name={'known[1]'} />
+						<Field type='text' style={{width: '20px'}} name={'known[2]'} />
+						<Field type='text' style={{width: '20px'}} name={'known[3]'} />
+						<Field type='text' style={{width: '20px'}} name={'known[4]'} />
+					</div>
+					<div>
+						<label htmlFor='butNotHere'>Not these positions</label>
+						<Field type='text' style={{width: '20px'}} name={'butNotHere[0]'} />
+						<Field type='text' style={{width: '20px'}} name={'butNotHere[1]'} />
+						<Field type='text' style={{width: '20px'}} name={'butNotHere[2]'} />
+						<Field type='text' style={{width: '20px'}} name={'butNotHere[3]'} />
+						<Field type='text' style={{width: '20px'}} name={'butNotHere[4]'} />
+					</div>
+					<div>
+						{dict.filter(word => regx.test(word)).map((filteredWord, idx) => (
+							<li key={idx}>
+								{filteredWord}
+							</li>
+						))}
+					</div>
+				</Form>
+			</Formik>
+		</div>
+	);
 }
 
 export default App;
